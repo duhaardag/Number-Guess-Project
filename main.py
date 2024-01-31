484574f53238117680b26ab608745db46d48b02d
duhaardag <ardagduha4@gmail.com>
2024-01-31 19:35:21 +0100

    first commit

--- main.py
+++ main.py
@@ -1,15 +1,15 @@
-print("Welcome To Number Guessing Game")
+print("Welcome To Number Guessing Game!")
 
 number = 32
 
 playerNumber = int(input("Guess a number between 0 and 50"))
 
-change = 3
+chance = 3
 
 while True:
-    if change != 0:
+    if chance != 0:
         if playerNumber > number:
-            change -= 1
-            print(change," you have the right left")
+            chance -= 1
+            print(chance," you have chance left")
             playerNumber = int(input("choose a smaller number: ") )
-            
+
