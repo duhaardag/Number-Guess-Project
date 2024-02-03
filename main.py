3ab44c5e7fabdf8ab43aa76882e0d464d947fd27
duhaardag <ardagduha4@gmail.com>
2024-02-03 12:04:54 +0100

    some changes

--- main.py
+++ main.py
@@ -1,15 +1,36 @@
-print("Welcome To Number Guessing Game!")
+import random
 
-number = 32
+def number_guessing_game():
 
-playerNumber = int(input("Guess a number between 0 and 50"))
+    min_number = 1
+    max_number = 50
+    target_number = random.randint(min_number, max_number)
 
-chance = 3
+    chance = 3
 
-while True:
-    if chance != 0:
-        if playerNumber > number:
-            chance -= 1
-            print(chance," you have chance left")
-            playerNumber = int(input("choose a smaller number: ") )
+    print("Welcome To Number Guessing Game!")
+    print(f"Guess a number between {min_number} and {max_number}.")
 
+    while chance > 0:
+        guess = int(input("Enter your guess: "))
+
+    if guess == target_number:
+        print("Congratulations! You guessed the correct number.")
+        break
+    elif guess < target_number:
+        print("Guess a big number")
+    else:
+        print("Guess a smaller number")
+
+        chance -= 1
+        if chance > 0:
+            print(f"You have {chance} chance{'s' if chance > 1 else ''} left.")
+        else:
+            print("Sorry, you've run out of chances. The correct number was:", target_number)
+
+if __name__ == "__main__":
+ number_guessing_game()
+
+
+        
+    
